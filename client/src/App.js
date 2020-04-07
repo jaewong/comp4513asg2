cc9e580df3fedb093e8299d4f2503d72c5f53790
LerisArandia <aaran577@mtroyal.ca>
2020-04-07 16:29:06 -0700

    # used cors fetch in moviedetailsview

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,6 @@ class App extends React.Component {
       const response = await fetch(url, options);
       const jsonData = await response.json({});
       console.log(jsonData);
-      // localStorage.setItem("movies", JSON.stringify(jsonData));
       this.setState({ movies: jsonData, loaded: true });
 
     } catch (error) {
