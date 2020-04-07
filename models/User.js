fc43b64f2bc449ff8dc1edbbdde05bb11072d2a4
jaewong <35346461+jaeemoo@users.noreply.github.com>
2020-04-07 01:10:16 -0600

    #testing

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // define a schema that maps to the structure of the data in MongoDB
 const userSchema = new mongoose.Schema({
