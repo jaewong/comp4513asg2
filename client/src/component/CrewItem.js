d0d4609f591ccd32d8196376ab7490f5704d5b94
jaewong <35346461+jaeemoo@users.noreply.github.com>
2020-04-07 05:02:38 -0600

    # Adding Ant Design

--- client/src/component/CrewItem.js
+++ client/src/component/CrewItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Row, Col, Divider } from 'antd';
 
 class CrewItem extends React.Component {
-    render(){
-        return(
-            <li key={this.props.key} className='tab-items'>
-                <div>{this.props.crew.department}</div>
-                <div>{this.props.crew.job}</div>
-                <div>{this.props.crew.name}</div>
-            </li>
+    render() {
+        return (
+            <>
+                <Row align="middle" key={this.props.key} className='tab-items'>
+                    <Col span='6' offset='2'>{this.props.crew.department}</Col>
+                    <Col span='6' offset='2'>{this.props.crew.job}</Col>
+                    <Col span='6' offset='2'>{this.props.crew.name}</Col>
+                </Row>
+                <Divider orientation="left" />
+            </>
         );
     }
 }
--- client/src/component/MovieDetails.js
+++ client/src/component/MovieDetails.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Ratings from './Ratings.js';
-import Modal from 'react-modal';
-import ModalHandler from './ModalHandler.js';
-import { Row, Col, Divider } from 'antd';
+import { Row, Col, Button, Typography, Modal, Card, Space, Descriptions, Tag, Divider} from 'antd';
 
 class MovieDetails extends React.Component {
     constructor(props) {
@@ -10,88 +8,120 @@ class MovieDetails extends React.Component {
         this.state = { movieData: [], loaded: false, showModal: false };
     }
 
-    openModal = () => { this.setState({ showModal: true }); }
-    closeModal = () => { this.setState({ showModal: false }); }
+    state = { visible: false };
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    hideModal = () => {
+        this.setState({
+            visible: false,
+        });
+    };
 
     render() {
+        const { Title } = Typography;
+        const { Meta } = Card;
+
         console.log(this.props.movieData.id);
         const imgUrl = `https://image.tmdb.org/t/p/w185/${this.props.movieData.poster}`;
         let companies, countries, keywords, genres = "";
         if (this.props.movieData.production.companies) {
             companies = this.props.movieData.production.companies.map((company, key) => {
-                return <div key={key} id="company">{company.name}</div>;
+                return <Tag color="magenta" key={key} id="company">{company.name}</Tag>;
             })
         } else { companies = <div>No company information available!</div>; }
 
         if (this.props.movieData.production.countries) {
             countries = this.props.movieData.production.countries.map((country, key) => {
-                return <div key={key} id="country">{country.name}</div>;
+                return <Tag color="magenta" key={key} id="country">{country.name}</Tag>;
             })
         } else { countries = <div>No country information available!</div>; }
 
         if (this.props.movieData.details.keywords) {
             keywords = this.props.movieData.details.keywords.map((keyword, key) => {
-                return < div key={key} id="keyword">#{keyword.name}</ div>;
+                return < Tag color="magenta" key={key} id="keyword">#{keyword.name}</Tag>;
             })
         } else { keywords = <div>No keywords available!</div> }
 
         if (this.props.movieData.details.genres) {
             genres = this.props.movieData.details.genres.map((genre, key) => {
-                return <div key={key} id="genre">{genre.name}</div>;
+                return <Tag color="magenta" key={key} id="genre">{genre.name}</Tag>;
             })
         } else { genres = <div>No genre information available!</div> }
 
         return (
             <Row id="details">
-                <Modal isOpen={this.state.showModal} overlayClassName="Overlay">
-                    <ModalHandler page={"details"} poster={this.props.movieData.poster} closeModal={this.closeModal}></ModalHandler>
+                <Modal 
+                    title="Poster"
+                    visible={this.state.visible}
+                    onCancel={this.hideModal}
+                    closable
+                    footer={null}
+                >
+                    <img src={imgUrl} id="modalPoster"/>
                 </Modal>
 
-                <Col flex="2" className="detailsCol">
-                    <h1>{this.props.movieData.title}</h1>
-                    <div id="poster">
-                        <img src={imgUrl} onClick={this.openModal} />
-                        <div>
-                            {this.props.movieData.tagline}<br />({this.props.movieData.runtime} mins)
-                        </div>
-                    </div>
+                <Col flex="2" className="detailsCol" id="poster">
+                    <Title>{this.props.movieData.title}</Title>
+                    <Card 
+                        id="poster"
+                        hoverable
+                        cover={<img src={imgUrl} />}
+                        onClick={this.showModal}
+                        style={{width: 400}}
+                    >
+                        <Meta title={this.props.movieData.tagline} />
+                        <div>{this.props.movieData.runtime} mins</div>
+                    </Card>
                 </Col>
-                <Col flex="3" className="detailsCol">
-                    <div style={{ textAlign: 'right' }}>
-                        <button onClick={this.addFav} >Add To Favorites</button>
-                    </div>
-                    <div id="details-details">
+                <Col flex="3" id="detailsD">
+                    <Button onClick={this.addFav} type="primary">Add To Favorites</Button>
+                    <Space id="details-details" direction="vertical">
                         <div>
-                            It was released in {this.props.movieData.release_date} and had a revenue of ${this.props.movieData.revenue}.<br />
-                            <a href={"https://www.themoviedb.org/movie/" + this.props.movieData.tmdb_id}>TMDB</a> <a href={"https://www.imdb.com/title" + this.props.movieData.imdb_id}>IMDB</a><br />
+                            It was released in {this.props.movieData.release_date} and had a revenue of ${this.props.movieData.revenue}.
+                            <Divider orientation="left">
+                                <Space>
+                                    <Button type="dashed" href={"https://www.themoviedb.org/movie/" + this.props.movieData.tmdb_id}>TMDB</Button> 
+                                    <Button type="dashed" href={"https://www.imdb.com/title" + this.props.movieData.imdb_id}>IMDB</Button>
+                                </Space>
+                            </Divider>
                         </div>
                         <div id='details-overview'>
                             {this.props.movieData.details.overview}
                         </div>
-                        <b>Popularity: </b>{this.props.movieData.ratings.popularity}<br />
-                        <b>Average Rating: </b><Ratings rating={this.props.movieData.ratings.average} />
-                        <b>Rating Count: </b>{this.props.movieData.ratings.count}
-                    </div>
+                        
+                        <Descriptions column={1} bordered>
+                            <Descriptions.Item label="Popularity">{this.props.movieData.ratings.popularity}</Descriptions.Item>
+                            <Descriptions.Item label="Average Rating"><Ratings rating={this.props.movieData.ratings.average} /> </Descriptions.Item>
+                            <Descriptions.Item label="Rating Count">{this.props.movieData.ratings.count}</Descriptions.Item>
+                        </Descriptions>
+                    </Space>
 
-                    Companies
-                    <div id="details-companies">
-                        {companies}
-                    </div>
+                    <Space id="tags" direction="vertical">
+                        <Title level={4}>Companies</Title>
+                        <div id="details-companies">
+                            {companies}
+                        </div>
 
-                    Countries
-                    <div id="details-countries">
-                        {countries}
-                    </div>
+                        <Title level={4}>Countries</Title>
+                        <div id="details-countries">
+                            {countries}
+                        </div>
 
-                    Keywords
-                    <div id="details-keywords">
-                        {keywords}
-                    </div>
+                        <Title level={4}>Keywords</Title>
+                        <div id="details-keywords">
+                            {keywords}
+                        </div>
 
-                    Genre
-                    <div id="details-genres">
-                        {genres}
-                    </div>
+                        <Title level={4}>Genre</Title>
+                        <div id="details-genres">
+                            {genres}
+                        </div>
+                    </Space>
                 </Col>
             </Row >
         );
