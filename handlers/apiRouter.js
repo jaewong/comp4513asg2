d7eefd96240f87a0014f649689dd7ef4a126077c
LerisArandia <aaran577@mtroyal.ca>
2020-04-07 03:12:19 -0700

    # temp no auth

--- handlers/apiRouter.js
+++ handlers/apiRouter.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 
 const router = express.Router();
 
-router.get('/users/:id', helper.ensureAuthenticated, (req, resp) => {
+router.get('/users/:id', (req, resp) => {
    UserModel.find({ id: req.params.id }, (err, data) => {
       if (err) {
          resp.json({ message: 'User not found' });
@@ -19,7 +19,7 @@ router.get('/users/:id', helper.ensureAuthenticated, (req, resp) => {
 
 });
 
-router.get('/movies', helper.ensureAuthenticated, (req, resp) => {
+router.get('/movies', (req, resp) => {
    MovieModel.find({}, (err, data) => {
       if (err) {
          resp.json({ message: 'Unable to connect to Movies' });
@@ -30,7 +30,7 @@ router.get('/movies', helper.ensureAuthenticated, (req, resp) => {
    });
 });
 
-router.get('/movies/:id', helper.ensureAuthenticated, (req, resp) => {
+router.get('/movies/:id', (req, resp) => {
    MovieModel.find({ id: req.params.id }, (err, data) => {
       if (err || data.length === 0) {
          resp.json({ message: 'Movie not found' });
@@ -41,7 +41,7 @@ router.get('/movies/:id', helper.ensureAuthenticated, (req, resp) => {
 });
 
 //handle request for title substring 
-router.get('/find/title/:title', helper.ensureAuthenticated, (req, resp) => {
+router.get('/find/title/:title', (req, resp) => {
    MovieModel.find({ 'title': new RegExp(req.params.title, 'i') }, (err, data) => {
       if (err || data.length === 0) {
          resp.json({ message: "No movie containing " + req.params.title });
@@ -53,7 +53,7 @@ router.get('/find/title/:title', helper.ensureAuthenticated, (req, resp) => {
 })
 
 // handle request for rating
-router.get('/find/rating/:r1/:r2', helper.ensureAuthenticated, (req, resp) => {
+router.get('/find/rating/:r1/:r2', (req, resp) => {
    MovieModel.find().where('ratings.average')
       .gt(req.params.r1)
       .lt(req.params.r2)
@@ -68,7 +68,7 @@ router.get('/find/rating/:r1/:r2', helper.ensureAuthenticated, (req, resp) => {
 })
 
 // handle request for year
-router.get('/find/year/:y1/:y2', helper.ensureAuthenticated, (req, resp) => {
+router.get('/find/year/:y1/:y2', (req, resp) => {
    MovieModel.find().where('release_date')
       .gt(req.params.y1)
       .lt(req.params.y2)
@@ -82,7 +82,7 @@ router.get('/find/year/:y1/:y2', helper.ensureAuthenticated, (req, resp) => {
 })
 
 // handle movie brief
-router.get('/brief', helper.ensureAuthenticated, (req, resp) => {
+router.get('/brief', (req, resp) => {
    BriefModel.find({}, function (err, data) {
       if (err || data.length === 0) {
          resp.json({ message: 'Unable to retrieve brief movies.' });
